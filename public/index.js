10032038ec35a7dc69b25f13791f8e0e6ae8911e
joshgreiff <josh@greiff.org>
2022-04-27 15:02:47 -0400

    finished my first dApp

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-const getMoodBtnEl = document.getElementById("getMoodBtn")
-const setMoodBtnEl = document.getElementById("setMoodBtn")
+// const getMoodBtnEl = document.getElementById("getMoodBtn")
+// const setMoodBtnEl = document.getElementById("setMoodBtn")
 
-function getMood () {
-    console.log('hello')
-}
-function setMood () {
+// function getMood () {
+//     console.log('hello')
+// }
+// function setMood () {
 
-}
+// }
 
-getMoodBtnEl.addEventListener('click', getMood)
-setMoodBtnEl.addEventListener('click', setMood)
+// getMoodBtnEl.addEventListener('click', getMood)
+// setMoodBtnEl.addEventListener('click', setMood)
